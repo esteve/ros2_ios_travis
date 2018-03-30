913297e6fcccc9069a8965335d2595cd8e0da5a9
Esteve Fernandez <esteve@apache.org>
2018-03-30 13:01:16 +0200

    Added macOS

---
+++ ci-scripts/script_macos.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+mkdir -p ~/ros2_objc_ws/src
+cd ~/ros2_objc_ws
+vcs import ~/ros2_objc_ws/src < $TRAVIS_BUILD_DIR/ros2_objc_macos.repos
+touch ~/ros2_objc_ws/src/ros2/rcl_interfaces/test_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/shape_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/stereo_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/trajectory_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/visualization_msgs/AMENT_IGNORE
+cd ~/ros2_objc_ws/src/ros2/rosidl_typesupport
+patch -p1 < ../../ros2_objc/ros2_objc/rosidl_typesupport_ros2_objc.patch
+cd ~/ros2_objc_ws
+src/ament/ament_tools/scripts/ament.py build \
+    --use-xcode \
+    --symlink-install \
+    --isolated \
+    --cmake-args \
+        -DCMAKE_BUILD_TYPE=Release \
+    -- | tee /tmp/ament_build.log | xcpretty
+tail /tmp/ament_build.log
