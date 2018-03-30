6fc765dd6d4bd59176b3d2a2a44ffc983a86696c
Esteve Fernandez <esteve@apache.org>
2018-03-30 13:48:49 +0200

    Removed opencv

--- ci-scripts/before_install_macos.sh
+++ ci-scripts/before_install_macos.sh
@@ -13,8 +13,6 @@ brew install cppcheck tinyxml eigen pcre
 # install dependencies for Fast-RTPS if you are using it
 brew install asio tinyxml2
 
-brew install opencv
-
 sudo -H python3 -m pip install vcstool
 
 sudo -H python3 -m pip install argcomplete coverage empy flake8 flake8-blind-except flake8-builtins flake8-class-newline flake8-comprehensions flake8-deprecated flake8-docstrings flake8-import-order flake8-quotes mock nose pep8 pydocstyle pyflakes pyparsing pytest pytest-cov pytest-runner pyyaml setuptools vcstool
