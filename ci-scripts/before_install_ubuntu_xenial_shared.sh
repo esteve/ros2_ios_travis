de2f8a42acad46363bbc770cb0e7545ab8aecdf4
Esteve Fernandez <esteve@apache.org>
2018-04-01 10:28:34 +0200

    Added Ubuntu

---
+++ ci-scripts/before_install_ubuntu_xenial_shared.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+docker pull esteve/ros2-ubuntu-xenial-travisci:objc
---
+++ ci-scripts/script_ubuntu_xenial_shared.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+mkdir -p ~/ros2_objc_ws/src
+cd ~/ros2_objc_ws
+vcs import ~/ros2_objc_ws/src < $TRAVIS_BUILD_DIR/ros2_objc_macos.repos
+touch ~/ros2_objc_ws/src/ros2/rcl_interfaces/test_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/shape_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/stereo_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/trajectory_msgs/AMENT_IGNORE
+touch ~/ros2_objc_ws/src/ros2/common_interfaces/visualization_msgs/AMENT_IGNORE
+cd ~/ros2_objc_ws/src/ros2/rosidl_typesupport
+patch -p1 < ../../ros2_objc/ros2_objc/rosidl_typesupport_ros2_objc.patch
+cd ~/ros2_objc_ws
+docker run \
+    -v $PWD/ci-scripts/ci-scripts \
+    -v $PWD/ros2_objc_ws:/ros2_objc_ws \
+    -e UID=$(id -u) \
+    --rm \
+    esteve/ros2-ubuntu-xenial-travisci:objc \
+    sh -c "\
+        src/ament/ament_tools/scripts/ament.py build \
+            --use-xcode \
+            --symlink-install \
+            --isolated \
+            --cmake-args \
+            -DCMAKE_BUILD_TYPE=Release \
+        "
